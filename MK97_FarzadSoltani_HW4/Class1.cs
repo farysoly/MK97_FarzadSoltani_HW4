96c864fffffe2ff0b2cfa91c47d4e7419dbb27b5
FarySoly <FarySoly@gmail.com>
2023-05-24 22:48:09 +0430

    ret

---
+++ MK97_FarzadSoltani_HW4/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MK97_FarzadSoltani_HW4
+{
+    internal class Class1
+    {
+    }
+}
